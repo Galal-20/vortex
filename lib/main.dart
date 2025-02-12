7598ba58cec94f01246b7882885765a7e06969d7
En.Galal Ahmed <ga71387@gmail.com>
2025-02-12 17:40:50 +0200

    SignUp and login Successfully

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ void main() async{
       create: (context) {
         final authBloc = AuthBloc(authRepository: AuthRepository());
         if (isLoggedIn) {
-          authBloc.add(AutoLoginRequested());  // Auto-login event
+          authBloc.add(AutoLoginRequested());
         }
         return authBloc;
       },      child: MyApp(),
